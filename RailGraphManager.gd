f9d0970302252804197c0c7b73d5ea804402f4e7
Eoin Brennan <eoin264@hotmail.com>
2026-04-15 21:06:29 +0100

    New test track Add signal scenes to each junction Set signal based on actual junction direction

--- RailGraphManager.gd
+++ RailGraphManager.gd
@@ -3,6 +3,9 @@ extends Node
 
 signal junction_changed(exit_port: Node3D)
 
+var signal_scene: PackedScene = preload("res://FeatherSignal.tscn")
+var junction_signals := {}  # exit_port -> signal instance
+
 var ports = {}  # key: "segmentA:portA"
 var connections := {}  # adjacency list
 
@@ -21,14 +24,61 @@ func get_default_start_port() -> Node3D:
 	
 	return null
 
+func _ready() -> void:
+	print("REady")
+
 func initialise(track: Node, track_connections: Node, start_node: Node3D):
+	print("Initialise")
 	track_root = track
 	connections_source = track_connections
 	build_graph(track_root)
 	apply_connections(connections_source)
-	print(ports)
-	print(connections)
 	start_port = start_node
+
+	_build_signals()
+
+func _build_signals() -> void:
+	print("BUILD SIGNALS")
+	for s in junction_signals.values():
+		if is_instance_valid(s):
+			s.queue_free()
+	junction_signals.clear()
+
+	print("FOREACH")
+
+	print(connections.keys())
+
+	for exit_port in connections.keys():
+		var options = get_connections(exit_port)
+		print("PORT: ", exit_port, "OPTIONS: ", options.size())
+		if options.size() <= 1:
+			continue
+		
+		var exit_forward = exit_port.global_transform.basis.z.normalized()
+
+		var m_sig = signal_scene.instantiate()
+		track_root.add_child(m_sig)
+
+		m_sig.global_position = exit_port.global_position
+
+		# align signal to track forward direction
+		m_sig.look_at(m_sig.global_position + exit_forward, Vector3.UP)
+
+		junction_signals[exit_port] = m_sig
+
+		_update_signal(exit_port)
+	
+func _update_signal(exit_port: Node3D) -> void:
+	var m_sig = junction_signals.get(exit_port)
+	if m_sig == null:
+		return
+	
+	var target_port = get_active_connection(exit_port)
+	if target_port == null:
+		return
+	
+	var dir_label = _get_direction_label(exit_port, target_port)
+	m_sig.set_direction_by_string(dir_label)
 
 func build_graph(track: Node):
 	ports.clear()
@@ -109,6 +159,8 @@ func set_switch(exit_port: Node3D, target_port: Node3D) -> void:
 	active_switches[exit_port] = target_port
 	junction_changed.emit(exit_port)
 
+	_update_signal(exit_port)
+
 func get_active_connection(exit_port: Node3D) -> Node3D:
 	if exit_port in active_switches:
 		return active_switches[exit_port]
@@ -141,3 +193,32 @@ func get_junction_state(exit_port: Node3D) -> Dictionary:
 		"options": options,
 		"active_port": active
 	}
+
+func _get_direction_label(exit_port: Node3D, target_port: Node3D) -> String:
+	var forward = -exit_port.global_transform.basis.z.normalized()
+
+	var segment = target_port.get_parent()
+	if segment == null or segment.path == null:
+		return "straight"
+	
+	var curve = segment.path.curve
+	if curve == null:
+		return "straight"
+	
+	var xform = segment.get_sample_transform(curve.get_baked_length())
+	var dir = xform.basis.z.normalized()
+
+	var dot = forward.dot(dir)
+	var cross = forward.cross(dir)
+
+	print(segment)
+	print(dot)
+	print(cross)
+	print("")
+
+	if dot > 0.7:
+		return "straight"
+	elif cross.y > 0:
+		return "left"
+	else:
+		return "right"
